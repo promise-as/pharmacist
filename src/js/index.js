c39f6d107f58154efffda2c2c11d843190a3a7d1
HongBaojin <13809735212@163.com>
2019-05-10 00:54:02 +0800

    finish select

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+$(function () {
+  // 默认显示的option标签
+  $(".current-type").click(function () {
+    $(".types-options").css("display", "block");
+
+    // 点击默认显示的option标签，就显示模拟的options
+    $(".types-options li").each(function () {
+      $(this).click(function () {
+        // 取模拟的select的li的rel的值赋值给option标签的value
+        $(".types-select").val($(this).attr("rel"));
+        // 隐藏模拟的select
+        $(".types-options").css("display", "none");
+        // 把从option标签的html值赋值给默认显示的option
+        $(".current-type").html($(this).html());
+      });
+    });
+  });
+
+  // 默认显示的option标签
+  $(".current-degree").click(function () {
+    $(".degree-options").css("display", "block");
+
+    // 点击默认显示的option标签，就显示模拟的options
+    $(".degree-options li").each(function () {
+      $(this).click(function () {
+        // 取模拟的select的li的rel的值赋值给option标签的value
+        $(".degree-select").val($(this).attr("rel"));
+        // 隐藏模拟的select
+        $(".degree-options").css("display", "none");
+        // 把从option标签的html值赋值给默认显示的option
+        $(".current-degree").html($(this).html());
+      });
+    });
+  });
+})
